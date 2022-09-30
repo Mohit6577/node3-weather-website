24178eb830d9158f009cb1fc3768f48e2d88cb9b
Mohit <Mohitchoudhary657@gmail.com>
2022-10-01 03:28:33 +0530

    forecast added about humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude,longitude,callback) => {
         }else if (body.error){
             callback('Unable to find forecast.write correct coordinates',undefined)
         }else{
-            callback(undefined,body.current.weather_descriptions[0]+' It is currently '+body.current.temperature+' degrees out. And it feels like '+body.current.feelslike)
+            callback(undefined,body.current.weather_descriptions[0]+' It is currently '+body.current.temperature+' degrees out. And it feels like '+body.current.feelslike+ "degrees out. The humidity is " + body.current.humidity + "%")
         }
     })
 }
